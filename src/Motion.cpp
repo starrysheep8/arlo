ca0a29e6c4ba94583a0f5faaaa63bb13735030c9
Reese Howell <reesehowell1@gmail.com>
2026-06-09 15:21:19 -0700

    define modes of motion in getCurrentAngle()

--- src/Motion.cpp
+++ src/Motion.cpp
@@ -42,20 +42,34 @@ class Motion {
 
         return timeSinceStart /= 1000000.0f; //convert from microseconds to seconds 
     }
+
+    float lerp(float t, float min, float max) {
+        return t * (max - min) + min;
+    }
  
     float getCurrentAngle() {
+        float time = getTimeSinceStart();
+        if (mode != SET_SPEED && time >= speedTime) return endAngle;
+
         switch (mode) {
-            case ROBOTIC:
-               break;
+            case ROBOTIC: //linear interpolation
+                return lerp(time / speedTime, startAngle, endAngle); 
             
-            case NATURAL:
-                break;
+            case NATURAL: //smooth step, x^2 (3 - 2x)
+                float x = time / speedTime;
+                float smoothStepValue = x * x * (3 - 2 * x);
+                return lerp(smoothStepValue, startAngle, endAngle);
 
-            case BOUNCE:
-                break;
+            case BOUNCE: //bounce with damped harmonic oscillator 1 - exp(-ζωt) * cos(ωt * sqrt(1 - ζ^2))
+                const float zeta = 0.75f; //damp strength
+                const float omega = 7.0f; //speed
+                float x = time / speedTime;
+                float dampedHarmonicValue = 1.0f - (float)exp(-zeta * omega * x) * cos(omega * x * sqrt(1.0f - zeta * zeta));
+                return lerp(dampedHarmonicValue, startAngle, endAngle);
 
-            case SET_SPEED:
-                break;
+            case SET_SPEED: //constant speed
+                return speedTime * speedMultiplier * time + startAngle;
+                
         }
     }
  
